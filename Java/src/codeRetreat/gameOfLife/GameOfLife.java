d6e9fdd5700747fe9f84c56516158d74dcd6a008
Jirka Pénzeš <jirkapenzes@hotmail.com>
2012-12-08 02:10:34 +0100

    Fix jUnit (use jUnit 4)

---
+++ Java/src/codeRetreat/gameOfLife/GameOfLife.java
@@ -0,0 +1,9 @@
+package codeRetreat.gameOfLife;
+
+/**
+ * Author: Jirka Pénzeš
+ * Date: 8.12.12
+ * Time: 2:05
+ */
+public class GameOfLife {
+}
--- Java/src/codeRetreat/tests/GameOfLifeTest.java
+++ Java/src/codeRetreat/tests/GameOfLifeTest.java
@@ -1,23 +1,18 @@
 package codeRetreat.tests;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Author: Jirka Pénzeš
  * Date: 8.12.12
- * Time: 1:02
+ * Time: 2:06
  */
-public class GameOfLifeTest extends TestCase {
+public class GameOfLifeTest  {
 
-    /*
-    1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-	2. Any live cell with two or three live neighbours lives on to the next generation.
-	3. Any live cell with more than three live neighbours dies, as if by overcrowding.
-	4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-    */
-
-    public void gameOfLifeTest() throws Exception {
-        assertEquals(true, true);
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
     }
-
 }
